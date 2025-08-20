c80d8dcdfa1e42e748fb698db089020a33c464aa
Diego Sol <diegosol127@gmail.com>
2025-08-19 23:20:42 -0600

    Reorganize files
    - Rename config folder and keymaps
    - Move core options into options.lua
    - Require lua files in init.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
+-- Set <Space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 -- Keybinds to make split navigation easier
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
