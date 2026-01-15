83e5404b23452246d8bca74d53819bc85a0075d1
Diego Sol <dmsol@sandia.gov>
2026-01-15 16:22:18 -0700

    Initial attempt at using lsp to make error detection more like an IDE

--- lua/plugins/core/lsp.lua
+++ lua/plugins/core/lsp.lua
@@ -14,8 +14,42 @@ return {
         },
       },
     },
-    vim.lsp.config("lua_ls", {}),
-    vim.lsp.enable("lua_ls")
+    config = function()
+
+      -- Global diagnostics config
+      vim.diagnostic.config({
+        virtua_text = {
+          prefix = "●",
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = "always",
+        },
+      })
+
+      -- Lua language server
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      -- Enable the server
+      vim.lsp.enable("lua_ls")
+    end,
   },
 }
 
