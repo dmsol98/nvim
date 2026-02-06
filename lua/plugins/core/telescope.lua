08ce07b5752a9ef0afc967a0bf77a84b68bd75fe
Diego Sol <dmsol@sandia.gov>
2026-02-05 17:46:45 -0700

    Add Telescope search queries
    - Rename some of the keymaps
    - Telescope search plugin files

--- lua/plugins/core/telescope.lua
+++ lua/plugins/core/telescope.lua
@@ -27,11 +27,11 @@ return {
       telescope.load_extension('fzf')
 
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>fp', function()
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope [F]ind [F]iles in CWD' })
+      vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Telescope [F]ind [A]ll in CWD' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope [F]ind [B]uffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope [F]ind [H]elp Tags' })
+      vim.keymap.set('n', '<leader>fi', function()
         local opts = require("telescope.themes").get_dropdown({
           layout_config = {
             width = 0.80,
@@ -39,10 +39,13 @@ return {
           },
         })
         builtin.current_buffer_fuzzy_find(opts)
-      end, { desc = 'Telescope find in current buffer' })
+      end, { desc = 'Telescope [F]ind [I]n Current Buffer' })
       vim.keymap.set('n', '<leader>fc', function()
         builtin.find_files { cwd = vim.fn.stdpath("config") }
-      end, { desc = 'Telescope find config files' })
+      end, { desc = 'Telescope [F]ind [P]lugin Files' })
+      vim.keymap.set('n', '<leader>fp', function()
+        builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }
+      end, { desc = 'Telescope [F]ind [C]onfig Files' })
     end,
 
   },
