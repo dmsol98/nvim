440858707fc7d84422b3f7507533d74699f279f3
Diego Sol <dmsol@sandia.gov>
2026-02-12 14:40:07 -0700

    Update lua formatting of config files

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -15,16 +15,16 @@ return {
       for _, lang in ipairs(lang_custom) do
         table.insert(lang_all, lang)
       end
-      require'nvim-treesitter.configs'.setup {
+      require 'nvim-treesitter.configs'.setup {
         ensure_installed = lang_all,
         highlight = {
           enable = true,
           disable = function(lang, buf)
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then
-                  return true
-              end
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
           end,
           additional_vim_regex_highlighting = false,
         },
