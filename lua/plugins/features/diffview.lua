7f9ad77784395fce6669436517dd5ebaab0008f2
Diego Sol <dmsol@sandia.gov>
2026-02-25 11:34:27 -0700

    Add extra brackets

--- lua/plugins/features/diffview.lua
+++ lua/plugins/features/diffview.lua
@@ -1,7 +1,9 @@
 return {
-  "sindrets/diffview.nvim",
-  config = function()
-    vim.keymap.set('n', '<leader>do', '<CMD>DiffviewOpen<CR>', { desc = '[D]iffview [O]pen' })
-    vim.keymap.set('n', '<leader>dc', '<CMD>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' })
-  end,
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      vim.keymap.set('n', '<leader>do', '<CMD>DiffviewOpen<CR>', { desc = '[D]iffview [O]pen' })
+      vim.keymap.set('n', '<leader>dc', '<CMD>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' })
+    end,
+  },
 }
