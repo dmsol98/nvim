4f7e9a6cafb1d62581eaeb30dffe204f54569590
Diego Sol <dmsol@sandia.gov>
2026-04-20 15:22:39 -0600

    Update window split resizing commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,8 +19,8 @@ set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Control the size of window splits
-set("n", "<M-,>", "<C-W>5>")
-set("n", "<M-.>", "<C-W>5<")
+set("n", "<M-,>", "<C-W>5<")
+set("n", "<M-.>", "<C-W>5>")
 set("n", "<M-'>", "<C-W>+")
 set("n", "<M-/>", "<C-W>-")
 
