7ee6b21fa87e8392912b47094c157043eb2126f8
Diego Sol <diegosol127@gmail.com>
2026-07-07 20:30:12 -0600

    Update tree-sitter config to fix API mismatch
    - Now point to `main` instead of `master`
    - Compatible with Neovim 0.12.0 or later

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -1,34 +1,46 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'main',
+  lazy = false,
+  build = ":TSUpdate",
 
-    config = function()
-      local lang_required = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
-      local lang_custom = { "bash", "cmake", "cpp", "json", "matlab", "sql", "toml", "xml", "yaml" }
-      local lang_all = {}
-      for _, lang in ipairs(lang_required) do
-        table.insert(lang_all, lang)
-      end
-      for _, lang in ipairs(lang_custom) do
-        table.insert(lang_all, lang)
-      end
-      require 'nvim-treesitter.configs'.setup {
-        ensure_installed = lang_all,
-        highlight = {
-          enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-          additional_vim_regex_highlighting = false,
-        },
-      }
-    end,
-  }
+  config = function()
+    local ts = require("nvim-treesitter")
+
+    -- Languages
+    local lang_required = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
+    local lang_custom = { "bash", "cmake", "cpp", "json", "matlab", "sql", "toml", "xml", "yaml" }
+
+    -- Concatenate languages to install
+    local lang_all = {}
+    for _, lang in ipairs(lang_required) do
+      table.insert(lang_all, lang)
+    end
+    for _, lang in ipairs(lang_custom) do
+      table.insert(lang_all, lang)
+    end
+
+    -- File types
+    local ft_required = { "c", "lua", "vim", "vimdoc", "query", "markdown" }
+    local ft_custom = { "bash", "cmake", "cpp", "json", "matlab", "sql", "toml", "xml", "yaml" }
+
+    -- Concatenate file types to install
+    local ft_all = {}
+    for _, ft in ipairs(ft_required) do
+      table.insert(ft_all, ft)
+    end
+    for _, ft in ipairs(ft_custom) do
+      table.insert(ft_all, ft)
+    end
+
+    ts.install( lang_all )
+
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = ft_all,
+        callback = function()
+          vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+  end,
 }
